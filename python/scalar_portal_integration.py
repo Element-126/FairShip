8b43f0464e9bda64e8af46f1cd8dfe06aec3eaf5
Jean-Loup Tastet <jeanloup@nbi.ku.dk>
2019-03-10 01:23:21 +0100

    Properly rescale the heavy meson branching ratios to the Scalar.
    The branching ratios of SM heavy mesons to the Scalar particle are typically
    small, therefore we need to rescale them to make event generation practical.
    This is done by applying a uniform scaling such that the largest total
    branching ratio of a given heavy meson to the Scalar is equal to unity.
    
    In order to ensure that PYTHIA does not rescale to unity the total branching
    ratio of each heavy meson to the Scalar, dummy decay channels to unphysical
    states are introduced, such that the uniform scaling is preserved for the final
    PYTHIA branching ratios. Placeholder particles of matching charges, with PDG
    codes 9900080 and ±9900081, are used to tag these dummy channels.

--- python/scalar_portal_integration.py
+++ python/scalar_portal_integration.py
@@ -3,44 +3,92 @@
 from __future__ import absolute_import, division
 from future.utils import viewitems
 
+from collections import OrderedDict
 import numpy as np
 
 from scalar_portal import *
+from scalar_portal.data.particles import get_charge, get_pdg_id
+
+
+_placeholder_particles = {
+     0: ('r0', 'r0', +9900080),
+    +1: ('r+', 'r-', +9900081),
+    -1: ('r-', 'r+', -9900081)
+}
+
+def _pythia_dummy_particle_strings():
+    return OrderedDict((name, format_pythia_particle_string(
+        pdg_id=pdg_id, name=name, antiname=antiname, spin_type=1,
+        charge_type=3*charge, mass=0., lifetime_si=0., new=True,
+        may_decay=False, is_visible=False))
+            for charge, (name, antiname, pdg_id) in viewitems(_placeholder_particles)
+            if charge >= 0)
+
+def _pythia_complementary_string(parent, branching_ratio):
+    '''
+    Dummy channels are used to prevent PYTHIA from rescaling the total
+    branching ratio of each heavy meson to 1, hence preserving the relative
+    production widths for the scalar.
+
+    They cause the heavy mesons to decay to some placeholder state with a
+    branching ratio complementary (1-BR) to the process it wraps. These
+    placeholder states can then be discarded during the analysis.
+    '''
+    charge = get_charge(parent)
+    _, _, placeholder_id = _placeholder_particles[charge]
+    return format_pythia_string(get_pdg_id(parent), [placeholder_id], branching_ratio)
 
 
 class RescaledProductionBranchingRatios(ProductionBranchingRatios):
     '''
     Specialization of `scalar_portal.ProductionBranchingRatios` with extra
     quirks to rescale the branching ratios for FairShip usage.
-
-    TODO: add dummy particles.
     '''
     def __init__(self, *args, **kwargs):
         super(RescaledProductionBranchingRatios, self).__init__(*args, **kwargs)
+        self._parent_particles = set(ch._parent for ch in self._channels.values())
+        self._channels_by_parent = OrderedDict(
+            (parent, [str(ch) for ch in self._channels.values() if ch._parent == parent])
+            for parent in self._parent_particles)
+        self._sum_br = OrderedDict(
+            (parent, sum(self._br[ch] for ch in self._channels_by_parent[parent]))
+            for parent in self._parent_particles)
         # Ref: https://stackoverflow.com/a/39279912
-        self._max_br = np.fmax.reduce(self._br.values())
-        self._rescaled_br = {
-            st: br / self._max_br for st, br in viewitems(self._br)}
+        self._max_sum_br = np.fmax.reduce(self._sum_br.values())
+        self._rescaled_br = OrderedDict(
+            (st, br / self._max_sum_br) for st, br in viewitems(self._br))
+        self._complementary_channels = OrderedDict(
+            (parent, 1 - sum_br/self._max_sum_br)
+            for parent, sum_br in viewitems(self._sum_br))
 
     @property
     def branching_ratios(self):
         return self._rescaled_br
 
     @property
-    def maximum_branching_ratio(self):
-        return self._max_br
+    def maximum_total_branching_ratio(self):
+        return self._max_sum_br
 
     @property
     def scaling_factor(self):
-        return 1 / self.maximum_branching_ratio
+        return 1 / self.maximum_total_branching_ratio
 
+    def pythia_strings(self):
+        dummy_particle_strs = _pythia_dummy_particle_strings()
+        base_strs = super(RescaledProductionBranchingRatios, self).pythia_strings()
+        complementary_strs = OrderedDict(
+            ('{} (rejected)'.format(parent), _pythia_complementary_string(parent, br))
+            for parent, br in viewitems(self._complementary_channels)
+            if br > 0)
+        all_strs = dummy_particle_strs.copy()
+        all_strs.update(base_strs)
+        all_strs.update(complementary_strs)
+        return all_strs
 
 class FairShipScalarModel(Model):
     '''
     Specialization of `scalar_portal.Model` with extra FairShip-specific
     features.
-
-    TODO: add dummy particles and channels.
     '''
     def __init__(self, scalar_id=9900025):
         super(FairShipScalarModel, self).__init__(scalar_id)
